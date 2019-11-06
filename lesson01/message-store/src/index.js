e5e33771de38779b1a160c408f3727cda7892a66
Peter Hooper <diversemix@gmail.com>
2019-11-06 15:15:43 +0000

    remove rougue js files

--- lesson01/message-store/src/index.js
+++
@@ -1,7 +0,0 @@
-import { MessageStoreImplementation } from './message-store';
-const log = console;
-log.log('Starting...');
-const ms = new MessageStoreImplementation();
-const file = ms.save(123, 'message1');
-log.log(`    written to ${file}`);
-log.log('Done!');
